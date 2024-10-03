0e6e212b8eecec6796e409ef216914cc93c20d6b
Hamza <hamza@Hamzas-MacBook-Air.local>
2024-10-03 13:26:13 +0500

    update

--- src/app/hooks/useEditPatient.ts
+++ src/app/hooks/useEditPatient.ts
@@ -5,7 +5,6 @@ export const useEditPatient = (): UseMutationResult<any, Error, any> => {
   const mutationResult = useMutation({
     mutationFn: async (payload: { id: string; data: any }) => {
       const { id, data } = payload;
-      console.log({data});
       const response = await axiosInstance.put<any>(`/api/patient/${id}`, data);
       return response.data;
     },
